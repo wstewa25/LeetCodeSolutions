e06f0ce44318927348699fea54906a3488203fc1
William Stewart <wstewa25@uncc.edu>
2021-11-13 13:28:25 -0500

    added better answer for question 1

--- JS/1.js
+++ JS/1.js
@@ -4,13 +4,29 @@
 //  * @return {number[]}
 //  */
 
- var twoSum = function(nums, target) {
+//First Answer, not as fast as second answer
+//  var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i+1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] == target) {
+//                 var resultArray = new Array(i, j);
+//                 return resultArray;
+//             }
+//         }
+//     }
+// };
+
+//Final Answer
+var twoSum = function(nums, target) {
+    let newArray = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                var resultArray = new Array(i, j);
-                return resultArray;
-            }
+        let num = nums[i];
+        let diff = target-num;
+        if (newArray[diff] !== undefined) {
+            let resultArray = new Array(newArray[diff], i);
+            return resultArray;
         }
+        newArray[num] = i;
     }
+    return [];
 };
